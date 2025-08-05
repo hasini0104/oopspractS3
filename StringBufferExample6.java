836fdaa1b57387b3dda553e92f9899128f12a74a
hasini0104 <komalihasini7@gmail.com>
2025-08-05 14:46:10 +0530

    Add files via upload

---
+++ StringBufferExample6.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class StringBufferExample6 {
+
+ 
+   public static void main(String args[]){  
+     StringBuffer sb=new StringBuffer();  
+     System.out.println(sb.capacity());//default 16  
+     sb.append("Hello");  
+     System.out.println(sb.capacity());//now 16  
+     sb.append("java is my favourite language");  
+     System.out.println(sb.capacity());
+   
+   
+ }
+
+}
