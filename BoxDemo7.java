8e8acfbe80984c19f1b1834202be7abd511d154b
hasini0104 <komalihasini7@gmail.com>
2025-08-12 14:56:33 +0530

    Add files via upload

---
+++ BoxDemo7.java
@@ -0,0 +1,35 @@
+package co2lab;
+class Box 
+{  
+  double width;
+  double height;
+  double depth;
+
+Box(double w, double h, double d)  //parameterized
+   {   
+  width = w; height = h; depth = d;
+   }
+
+double volume()
+  { 
+  return width * height * depth;
+   }
+}
+class BoxDemo7
+ {
+ public static void main(String args[]) 
+  { 
+    // declare, allocate, and initialize Box objects
+  Box mybox1 = new Box(10, 20, 15); 
+  Box mybox2 = new Box(3, 6, 9); 
+  double vol; 
+  
+  // get volume of first box 
+  vol = mybox1.volume(); 
+  System.out.println("Volume is " + vol);
+  
+   // get volume of second box 
+  vol = mybox2.volume(); 
+  System.out.println("Volume is " + vol);
+ }
+ }
